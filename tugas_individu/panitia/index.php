2256117a2e911b3c17f159ee822af6a42980c427
aliwarnobinsaiman@gmail.com <aliwarnobinsaiman@gmail.com>
2023-07-30 23:36:38 +0700

    update file

--- tugas_individu/panitia/index.php
+++ tugas_individu/panitia/index.php
@@ -5,22 +5,9 @@
   <link rel="stylesheet" href="../css/styles.css">
 </head>
 <body>
-<div class="custom-table">
-    <div class="table-header">
-      <div class="header-item">Nama</div>
-      <div class="header-item">Usia</div>
-      <div class="header-item">Kota</div>
-    </div>
-    <div class="table-row">
-      <div class="table-cell">John Doe</div>
-      <div class="table-cell">30</div>
-      <div class="table-cell">New York</div>
-    </div>
-    <div class="table-row">
-      <div class="table-cell">Jane Smith</div>
-      <div class="table-cell">25</div>
-      <div class="table-cell">London</div>
-    </div>
-  </div>
+  <?php
+  
+   include("view.php");
+  ?>
 </body>
 </html>
---
+++ tugas_individu/panitia/view.php
@@ -0,0 +1,26 @@
+<div class="conten">
+  <div class="container">
+    <div class="custom-table">
+      <div class="table-header">
+        <div class="header-item">NO</div>
+        <div class="header-item">Nama</div>
+        <div class="header-item">Bagian</div>
+      </div>
+      <div class="table-row">
+        <div class="table-cell">1</div>
+        <div class="table-cell">Ali</div>
+        <div class="table-cell">Bendahara</div>
+      </div>
+      <div class="table-row">
+        <div class="table-cell">2</div>
+        <div class="table-cell">Jono</div>
+        <div class="table-cell">Sekretaris</div>
+      </div>
+      <div class="table-row">
+        <div class="table-cell">3</div>
+        <div class="table-cell">Haryono</div>
+        <div class="table-cell">Anggota</div>
+      </div>
+    </div>
+    </div>
+</div>
